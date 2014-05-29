107c23a054bcd605ee76b4138bc5dcf74cf84846
Geoffrey Corey <geoffcorey7@gmail.com>
2014-05-29 14:49:56 -0700

    Adding server starting/stopping for api related tests

--- test/test-search.js
+++ test/test-search.js
@@ -2,8 +2,13 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
 var api = request('http://localhost:3000');
+var server = require('../app.js');
 
 describe('Search', function(){
+
+  before(function(){
+    server.listen(3000);
+  });
 
   describe('GET /search', function() {
     it('should return 400 for bad query', function(done){
@@ -31,5 +36,9 @@ describe('Search', function(){
         done();
       });
     });
+  });
+
+  after(function(){
+    server.close();
   });
 });
--- test/test-store.js
+++ test/test-store.js
@@ -2,3 +2,16 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
 var api = request('http://localhost:3000');
+var server = require('../app.js');
+
+describe('Search', function(){
+
+  before(function(){
+    server.listen(3000);
+  });
+
+
+  after(function(){
+    server.close();
+  });
+});
